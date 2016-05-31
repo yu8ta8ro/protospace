0d1796add154dfd3e21e9454080bac5fab05c613
森田雄太郎 <yu8ta8ro@icloud.com>
2016-05-31 18:24:17 +0900

    added the function of browsing and  editing user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
   root 'prototypes#index'
   resources :prototypes, only: [:index, :new, :show]
 end
