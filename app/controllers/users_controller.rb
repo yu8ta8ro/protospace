c7b99166481f499c7d25e1c150c5d87589ccab3d
森田雄太郎 <yu8ta8ro@icloud.com>
2016-06-04 14:45:56 +0900

    edit,update and header-view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+   def edit
+  end
+
+  def update
+    current_user.update(update_params)
+    redirect_to controller: :prototypes, action: :index
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :email, :member, :profile, :works)
+  end
 end
