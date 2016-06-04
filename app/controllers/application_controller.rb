283f7f27a808a3ba2a57dec1ea2befdd542dfe53
森田雄太郎 <yu8ta8ro@icloud.com>
2016-06-04 23:31:33 +0900

    edit,update and header-view2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
-    '/users/sign_in'
+    new_user_session_path
   end
 
   def after_sign_in_path_for(resource)
-    '/'
+    prototypes_path
   end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :avater, :member, :profile, :works]
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
-   def edit
+
+  def edit
   end
 
   def update
     current_user.update(update_params)
-    redirect_to controller: :prototypes, action: :index
+    redirect_to prototypes_path
   end
 
   private
+
   def update_params
     params.require(:user).permit(:name, :email, :member, :profile, :works)
   end
+
 end
