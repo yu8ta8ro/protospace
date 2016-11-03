64fa5007a7f233f710b49fce302197191e61bba9
森田雄太郎 <yu8ta8ro@icloud.com>
2016-11-03 15:46:06 +0900

    Edit_method

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -19,6 +19,20 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.user_id == current_user.id
+      prototype.update(create_params)
+    end
+
+    redirect_to root_path, notice: "YOYOYO! Updated!"
+  end
+
+
   private
   def create_params
     params.require(:prototype).permit(:catch_copy, :title, :concept, :user_id,
