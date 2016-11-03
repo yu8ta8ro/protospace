20d228bb1483cb3398534dd47213044c34ce52a3
森田雄太郎 <yu8ta8ro@icloud.com>
2016-11-03 19:31:07 +0900

    Edit_method2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
 
+  before_action :prototype_id, only: :edit
+
   def index
     @prototypes = Prototype.order('id DESC')
   end
@@ -20,13 +22,11 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.user_id == current_user.id
-      prototype.update(create_params)
+    if @prototype.user_id == current_user.id
+      @prototype.update(create_params)
     end
 
     redirect_to root_path, notice: "YOYOYO! Updated!"
@@ -37,6 +37,10 @@ class PrototypesController < ApplicationController
   def create_params
     params.require(:prototype).permit(:catch_copy, :title, :concept, :user_id,
       prototype_images_attributes: [:id, :content, :content_type, :prototype_id])
+  end
+
+  def prototype_id
+    @prototype = Prototype.find(params[:id])
   end
 
 end
