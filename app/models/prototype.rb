187786a0c44152d1758400b1791e38a578431d5c
森田雄太郎 <yu8ta8ro@icloud.com>
2016-11-04 01:40:38 +0900

    Like_function

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,11 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images
   has_one :main_image, -> { where(content_type: 0) }, class_name: "PrototypeImage"
+  has_many :likes, dependent: :destroy
+
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
 
   validates :title, :concept, :catch_copy, :prototype_images, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630161157) do
+ActiveRecord::Schema.define(version: 20161103154553) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prototype_images", force: :cascade do |t|
     t.text     "content",      limit: 65535
