9b6af8a6d4002df333241d05128e5c9b18399432
森田雄太郎 <yu8ta8ro@icloud.com>
2016-11-17 14:43:48 +0900

    Like_function3

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,20 @@
 class LikesController < ApplicationController
+
+  before_action :prototype_id, only: [:create, :destroy]
+
   def create
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
+  end
+
+  private
+  def prototype_id
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
