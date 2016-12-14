a0cdaa87f8baa91b97c299ad193bc69ff0a53396
森田雄太郎 <yu8ta8ro@icloud.com>
2016-12-14 19:13:38 +0900

    js_preview2

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,15 @@
-$(function show_avatar_preview() {
-  $("#user_avatar").on("change", function() {var file = this.files;
-  if (!file[0].type.match(/^image\/(png|jpeg|gif)$/)) return;
-  var img = new Image();
-  var reader = new FileReader();
-  reader.onload = function (e) {$("#avatar_image_preview").attr('src', e.target.result).css('display', 'block');}
-  reader.readAsDataURL(file[0]);
- });
-});
+$ ->
+  $('#user_avatar').on 'change', ->
+    file = @files
+    if !file[0].type.match(/^image\/(png|jpeg|gif)$/)
+      return
+    img = new Image
+    reader = new FileReader
+
+    reader.onload = (e) ->
+      $('#avatar_image_preview').attr('src', e.target.result).css 'display', 'block'
+      return
+
+    reader.readAsDataURL file[0]
+    return
+  return
